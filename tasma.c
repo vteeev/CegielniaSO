7d7ee05ceb729b5bfbda7cbe06205e765d1080f3
Piotrek <piotr.nie00@gmail.com>
2025-01-12 23:22:25 +0100

    dodanie semfara do tasma.c

--- tasma.c
+++ tasma.c
@@ -1,40 +1,65 @@
 #include "tasma.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-Tasma* tasma = NULL;
-sem_t sem_tasma;
-
-void init_tasma() {
-    int shm_id = shmget(IPC_PRIVATE, sizeof(Tasma), IPC_CREAT | 0666);
-    if (shm_id == -1) {
-        perror("shmget failed");
-        exit(1);
-    }
-    tasma = (Tasma*)shmat(shm_id, NULL, 0);
-    if (tasma == (void*)-1) {
-        perror("shmat failed");
-        exit(1);
-    }
-    tasma->liczba_cegiel = 0;
+void Tasma_init(Tasma* tasma, int maks_liczba_cegiel, int maks_masa) {
+    tasma->maks_liczba_cegiel = maks_liczba_cegiel;
+    tasma->maks_masa = maks_masa;
     tasma->aktualna_masa = 0;
-    sem_init(&sem_tasma, 1, 1);
+    tasma->tasma = (int*)malloc(sizeof(int) * maks_liczba_cegiel); // Alokacja pamiêci na ceg³y
+    tasma->front = 0;
+    tasma->rear = 0;
+    tasma->size = 0;
+
+    // Inicjalizacja mutexu i zmiennej warunkowej
+    pthread_mutex_init(&tasma->mtx, NULL);
+    pthread_cond_init(&tasma->cv, NULL);
 }
 
-void dodaj_cegle(int masa) {
-    sem_wait(&sem_tasma);
-    if (tasma->liczba_cegiel < K && (tasma->aktualna_masa + masa) <= M) {
-        tasma->cegly[tasma->liczba_cegiel++].masa = masa;
-        tasma->aktualna_masa += masa;
+int Tasma_dodaj_cegle(Tasma* tasma, int masa_cegly) {
+    pthread_mutex_lock(&tasma->mtx);  // Zablokowanie mutexu
+
+    if (tasma->size < tasma->maks_liczba_cegiel && (tasma->aktualna_masa + masa_cegly) <= tasma->maks_masa) {
+        tasma->tasma[tasma->rear] = masa_cegly;
+        tasma->rear = (tasma->rear + 1) % tasma->maks_liczba_cegiel;
+        tasma->aktualna_masa += masa_cegly;
+        tasma->size++;
+
+        printf("Dodano ceg³ê o masie %d. Aktualna masa: %d.\n", masa_cegly, tasma->aktualna_masa);
+
+        pthread_cond_signal(&tasma->cv);  // Sygnalizowanie, ¿e ceg³a jest dostêpna
     }
-    sem_post(&sem_tasma);
+
+    pthread_mutex_unlock(&tasma->mtx);  // Odblokowanie mutexu
+    return (tasma->size < tasma->maks_liczba_cegiel) ? 1 : 0;  // Jeœli dodano ceg³ê, zwróæ 1
 }
 
-int zdejmij_cegle(int ladownosc) {
-    int ladunek = 0;
-    sem_wait(&sem_tasma);
-    while (ladunek < ladownosc && tasma->liczba_cegiel > 0) {
-        ladunek += tasma->cegly[--tasma->liczba_cegiel].masa;
-        tasma->aktualna_masa -= tasma->cegly[tasma->liczba_cegiel].masa;
+int Tasma_pobierz_cegle(Tasma* tasma) {
+    pthread_mutex_lock(&tasma->mtx);  // Zablokowanie mutexu
+
+    while (tasma->size == 0) {  // Czekanie, jeœli taœma jest pusta
+        pthread_cond_wait(&tasma->cv, &tasma->mtx);  // Czekanie na dostêpnoœæ ceg³y
     }
-    sem_post(&sem_tasma);
-    return ladunek;
+
+    int masa_cegly = tasma->tasma[tasma->front];
+    tasma->front = (tasma->front + 1) % tasma->maks_liczba_cegiel;
+    tasma->aktualna_masa -= masa_cegly;
+    tasma->size--;
+
+    printf("Pobrano ceg³ê o masie %d. Pozosta³a masa: %d.\n", masa_cegly, tasma->aktualna_masa);
+
+    pthread_mutex_unlock(&tasma->mtx);  // Odblokowanie mutexu
+    return masa_cegly;
+}
+
+int Tasma_sprawdz_cegle(Tasma* tasma) {
+    pthread_mutex_lock(&tasma->mtx);  // Zablokowanie mutexu
+
+    int masa_cegly = 0;
+    if (tasma->size > 0) {
+        masa_cegly = tasma->tasma[tasma->front];
+    }
+
+    pthread_mutex_unlock(&tasma->mtx);  // Odblokowanie mutexu
+    return masa_cegly;
 }
--- tasma.h
+++ tasma.h
@@ -1,12 +1,25 @@
 #ifndef TASMA_H
 #define TASMA_H
 
-#include <semaphore.h>
-#include "cegielnia.h"
+#include <pthread.h>  // Do³¹czamy bibliotekê dla pthread
 
-// Funkcje dla taœmy
-void init_tasma();
-void dodaj_cegle(int masa);
-int zdejmij_cegle(int ladownosc);
+// Struktura reprezentuj¹ca taœmê
+typedef struct {
+    int maks_liczba_cegiel;
+    int maks_masa;
+    int aktualna_masa;
+    int* tasma;       // Tablica na przechowywanie cegie³
+    int front;        // Indeks pierwszej ceg³y w kolejce
+    int rear;         // Indeks ostatniej ceg³y w kolejce
+    int size;         // Liczba cegie³ w kolejce
+    pthread_mutex_t mtx;  // Mutex do synchronizacji dostêpu
+    pthread_cond_t cv;    // Zmienna warunkowa do synchronizacji
+} Tasma;
 
-#endif
+// Funkcje operuj¹ce na taœmie
+void Tasma_init(Tasma* tasma, int maks_liczba_cegiel, int maks_masa);
+int Tasma_dodaj_cegle(Tasma* tasma, int masa_cegly);
+int Tasma_pobierz_cegle(Tasma* tasma);
+int Tasma_sprawdz_cegle(Tasma* tasma);
+
+#endif // TASMA_H
