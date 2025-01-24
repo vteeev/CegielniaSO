d14be36ef66d56fa678da82ef0fe6f910020079a
Piotrek <piotr.nie00@gmail.com>
2025-01-24 15:53:17 +0100

    operacje: struktura Ciezarowka

--- operacje.h
+++ operacje.h
@@ -3,6 +3,39 @@
 
 #include <sys/types.h>
 #include <sys/ipc.h>
+#include <semaphore.h>
+#include <signal.h> // Dla sig_atomic_t
+
+// Deklaracje zmiennych globalnych
+
+// Sta³
+
+#define N 40
+#define P 3
+#define MAX_CAPACITY 20  // Pojemnoœæ taœmy
+#define C_COUNT 100
+#define SLEEP_TIME 20     // Czas uœpienia ciê¿arówki po odjeŸdzie
+#define CIEZAROWKA_LIMIT 1000  // Limit masy dla ciezarowki
+// Struktura Taœma (przechowuj¹ca dane i synchronizuj¹ca procesy)
+typedef struct {
+    int buffer[MAX_CAPACITY];  // Tablica przechowuj¹ca dane
+    int head;         // WskaŸnik na pocz¹tek taœmy
+    int tail;         // WskaŸnik na koniec taœmy
+    int suma;         // Suma ³adunku na taœmie
+    int count;        // Liczba elementów w buforze
+    int kolejnosc[C_COUNT];       // Tablica kolejnoœci ciê¿arówek
+    int front;                    // WskaŸnik na pocz¹tek kolejki (indeks pierwszej ciê¿arówki)
+    int rear;  //na koniec
+    int last_digit;   // Ostatni element, który zosta³ za³adowany przez ciê¿arówkê
+    int active_truck;
+} Tasma;
+
+typedef struct {
+    pid_t trucks[C_COUNT];  // Tablica przechowuj¹ca dane
+    int head;
+    int tail;
+    int active_truck2;
+} Ciezarowka;
 
 int create_sem(key_t key);
 int create_shm(key_t key, size_t size);
