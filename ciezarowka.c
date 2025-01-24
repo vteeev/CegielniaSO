64f59317adb8bee5e5a1750de83282d853fc84af
Piotrek <piotr.nie00@gmail.com>
2025-01-24 15:47:52 +0100

    ciezarowka: main, pamieć dzielona

--- ciezarowka.c
+++ ciezarowka.c
@@ -64,3 +64,73 @@ pid_t utworz_ciezarowke(int id) {
     }
     return pid;
 }
+
+int main()
+{
+
+
+    printf("KKKKKKKKKKKKKKKKKKKKKK\n");
+    key_t key_m1 = ftok(".", 'R');
+    if (key_m1 == -1) {
+        perror("ftok");
+        exit(1);
+    }
+    int shmid1 = shmget(key_m1, sizeof(Tasma), IPC_CREAT | 0600);
+    if (shmid1 == -1) {
+        perror("shmget");
+        exit(1);
+    }
+
+    Tasma* tasma = (Tasma*)shmat(shmid1, NULL, 0);
+    if (tasma == (void*)-1) {
+        perror("shmat");
+        exit(1);
+    }
+    printf("KKKKKKKKKKKKKKKKKKKKKK\n");
+    key_t key_m2 = ftok("/bin/ls", 'C');
+
+    if (key_m2 == -1) {
+        perror("ftok");
+        exit(1);
+    }
+    int shmid2 = shmget(key_m2, sizeof(Ciezarowka), IPC_CREAT | 0600);
+    if (shmid2 == -1) {
+        perror("shmgetttttt");
+        exit(1);
+    }
+    Ciezarowka* ciezarowka = (Ciezarowka*)shmat(shmid2, NULL, 0);
+    if (ciezarowka == (void*)-1) {
+        perror("shmattttt");
+        exit(1);
+    }
+    printf("KKKKKKKKKKKKKKKKKKKKKK\n");
+    key_t key_m3 = ftok("/bin/ls", 'F');
+
+    int shmid3 = shmget(key_m3, sizeof(SharedFlags), IPC_CREAT | 0600);
+    if (shmid3 == -1) {
+        perror("shmget");
+        exit(1);
+    }
+    SharedFlags* shared_f = (SharedFlags*)shmat(shmid3, NULL, 0);
+    if (shared_f == (void*)-1) {
+        perror("shmat");
+        exit(1);
+    }
+    printf("KKKKKKKKKKKKKKKKKKKKKK\n");
+    int semID;
+    key_t key_s = ftok("/bin/ls", 'S');
+    semID = semget(key_s, 10, IPC_CREAT | 0666); // 8 semaforów
+    if (semID == -1) {
+        perror("semget");
+        exit(1);
+    }
+
+
+
+    shmdt(tasma);  // Od³¹czenie pamiêci dzielonej
+    shmdt(ciezarowka);
+    shmdt(shared_f);
+
+
+    //shmctl(shmid, IPC_RMID, NULL);//usuniecie
+    return 0;
