0709d62eaf1a17e08df49f8ad5eba94ec127f848
Piotrek <piotr.nie00@gmail.com>
2025-01-27 21:15:17 +0100

    aktualizacja dyspozytor.h

--- dyspozytor.h
+++ dyspozytor.h
@@ -1,9 +1,16 @@
 #ifndef DYSPOZYTOR_H
 #define DYSPOZYTOR_H
 
-#include "cegielnia.h"
+#include <semaphore.h>
+#include <signal.h> // Dla sig_atomic_t
+#include "operacje.h"
 
-// Funkcja dla dyspozytora
-void dyspozytor();
 
-#endif
+
+char get_char_no_wait();
+char getch();
+void flush_input_buffer();
+//void kontroluj_sygnal1(struct Tasma* tasma, pid_t truck_pids[]);
+void kontroluj_sygnal2(pid_t prac_pids[]);
+
+#endif  //DYSPOZYTOR_H
