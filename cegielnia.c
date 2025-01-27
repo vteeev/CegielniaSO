44a55ae86b5e4bb7e73aec946858d7dfd79b56a4
Piotrek <piotr.nie00@gmail.com>
2025-01-27 21:09:34 +0100

    Main: dodanie bibliotek, (ID semafora, kolejki kom. pamieci dzielonej)

--- cegielnia.c
+++ cegielnia.c
@@ -1,4 +1,20 @@
-#include "cegielnia.h"
+#include <stdio.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <sys/sem.h>
+#include <signal.h>
+#include <sys/ipc.h>
+#include <fcntl.h>  // Dla O_WRONLY, O_CREAT, O_APPEND
+#include "operacje.h"
+//#include "operacje.c"
+
+int shmID, semID, msgID;  //ID semafora, kolejki kom. pamieci dzielonej
+
 
 int main() {
     init_tasma();
